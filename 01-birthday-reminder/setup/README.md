9ff1919537daa19b62ae08bdf829e1256e0aed5d
myildizCH <meyildiz.de@gmail.com>
2022-06-26 21:13:32 +0200

    01 - birthday reminder

--- 01-birthday-reminder/setup/README.md
+++ 01-birthday-reminder/setup/README.md
@@ -1,3 +1,10 @@
 ## Idea
 
 [https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
+
+## Lessons Learned
+
+1 - How to use props.
+2 - How to use useState.
+3 - How to handle a click event.
+4 - How to enable Emmit suggestions for react.
--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3> {people.length} birthdays today</h3>
+        <List people={people} />
+        {/* the code below clears tha passed array of people by replacing the array of people with an empty array */}
+        <button onClick={() => setPeople([])}>Clear All</button> 
+      </section>
+    </main>
+  );
 }
 
 export default App;
